311a7603ef3ff0eff422dc68c9a01c82340692cb
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-04-19 21:33:37 -0700

    rolled natural 20

--- envs.ts
+++ envs.ts
@@ -1,3 +1,5 @@
-export const DISCORD_CLIENT_ID = Deno.env.get("DISCORD_CLIENT_ID")!;
+export const DISCORD_APPLICATION_ID = Deno.env.get("DISCORD_APPLICATION_ID")!;
 
-export const DISCORD_CLIENT_SECRET = Deno.env.get("DISCORD_CLIENT_SECRET")!;
+export const DISCORD_TOKEN = Deno.env.get("DISCORD_TOKEN")!;
+
+export const DISCORD_PUBLIC_KEY = Deno.env.get("DISCORD_PUBLIC_KEY")!;
